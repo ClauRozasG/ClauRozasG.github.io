7ab289e3726523153d6142ded216495787ba87c4
ClauRozasG <u201922910@upc.edu.pe>
2023-05-22 00:49:38 -0500

    Added html, css y js

---
+++ program.js
@@ -0,0 +1,35 @@
+/*const textInicio = document.querySelector(".cardInput");
+const textOutput = document.querySelector(".cardOutput");*/
+
+function encriptar() {
+    var texto = document.querySelector(".cardInput").value;
+    let condicion = /^[a-z\s]+$/;
+    if(texto.length==0){
+        alert("No hay nada que pueda encriptar. Intentalo de nuevo");
+    }
+    else if(texto.match(condicion)){
+        var textoCifrado = texto.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
+        document.querySelector(".cardOutput").value = textoCifrado;
+        document.querySelector("#btn-encriptar").value;
+        document.querySelector(".cardInput").value="";
+    }
+    else{
+        alert("No se aceptan caracteres especiales, números o acentos");
+    }
+    
+}
+function desencriptar() {
+    var texto = document.querySelector(".cardInput").value;
+    if(texto.length==0){
+        alert("No hay nada que pueda desencriptar. Intentalo de nuevo");
+    }else{
+        var textoCifrado = texto.replace(/ai/gi, "a").replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ober/gi, "o").replace(/ufat/gi, "u");
+        document.querySelector(".cardOutput").value = textoCifrado;
+        document.querySelector("#btn-desencriptar").value;
+    }
+    
+}
+function copiar() {
+    var mensajeMostrado = document.querySelector(".cardOutput").value;
+    navigator.clipboard.writeText(mensajeMostrado);
+}
