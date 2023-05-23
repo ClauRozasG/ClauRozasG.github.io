7bb5b0703764cd43b1f332fd75a3c5925363469a
ClauRozasG <u201922910@upc.edu.pe>
2023-05-23 01:06:00 -0500

    Added responsive design and changed image names

--- program.js
+++ program.js
@@ -20,12 +20,17 @@ function encriptar() {
 }
 function desencriptar() {
     var texto = document.querySelector(".cardInput").value;
+    let condicion = /^[a-z\s]+$/;
     if(texto.length==0){
         alert("No hay nada que pueda desencriptar. Intentalo de nuevo");
-    }else{
+    }else if(texto.match(condicion)){
         var textoCifrado = texto.replace(/ai/gi, "a").replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ober/gi, "o").replace(/ufat/gi, "u");
         document.querySelector(".cardOutput").value = textoCifrado;
         document.querySelector("#btn-desencriptar").value;
+        document.querySelector(".cardInput").value="";
+    }
+    else{
+        alert("No se aceptan caracteres especiales, números o acentos");
     }
     
 }
